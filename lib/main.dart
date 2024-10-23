1b23a342aba8361714942586e82107dfeff84fa6
Emjay Ramento <ramento.emjay@gmail.com>
2024-10-24 00:55:25 +0800

    Describe the changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,23 @@
 import 'signup_screen.dart';
 import 'login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Auth Demo',
+      home: SignUpScreen(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   @override
